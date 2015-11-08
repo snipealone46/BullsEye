11517edfa0c19c5b38a028a7acd6a91f98a2073c
Eric Yang <eric.shaohuiyang@gmail.com>
2015-11-07 16:16:37 -0800

    Game will now generate random target value restart after "hit me"
    *used arc4random_uniform() to generate random num
    *game will restart after showing the alert
    *alert will contains the difference of target and slider value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 50
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,8 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var slider:UISlider!
+    
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
         
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
@@ -30,10 +33,17 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved (slider: UISlider){
         currentValue = lroundf(slider.value)
     }
+    
+    func startNewRound () {
+        currentValue = Int(lroundf(slider.value))
+        targetValue = 1 + Int(arc4random_uniform(100))
+    }
+    
 }
 
